c6534f8a558ab102026119c59c6cd0e8d55c98ac
D.Ibragimov <uzdevid@gmail.com>
2025-07-07 16:35:15 +0500

    load envs

--- autoload.php
+++ autoload.php
@@ -6,7 +6,7 @@ use Dotenv\Dotenv;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__, ['secrets.env', '.env']);
 $dotenv->load();
 
 $_ENV['YII_ENV'] = empty($_ENV['YII_ENV']) ? null : $_ENV['YII_ENV'];
