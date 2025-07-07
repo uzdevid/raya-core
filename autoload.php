8717c7c7693e98e180b3c8c0c3211a632b78c6b0
D.Ibragimov <uzdevid@gmail.com>
2025-07-07 17:19:22 +0500

    fix

--- autoload.php
+++ autoload.php
@@ -5,7 +5,7 @@ use Dotenv\Dotenv;
 require_once __DIR__ . '/vendor/autoload.php';
 
 Dotenv::createImmutable(__DIR__, ['secrets.env'])->load();
-Dotenv::createImmutable(__DIR__)->load();
+Dotenv::createImmutable(__DIR__, ['.env'])->load();
 
 $_ENV['YII_ENV'] = empty($_ENV['YII_ENV']) ? null : $_ENV['YII_ENV'];
 $_SERVER['YII_ENV'] = $_ENV['YII_ENV'];
